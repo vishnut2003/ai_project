95f5f67a45fb4304ba3d0ac17ef179dd78c4aba6
Vishnu <iam.vishnu.bhaskar@gmail.com>
2024-11-03 23:20:53 -0800

    Home page hero section finish in desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import HeroSection from "@/components/HomePage/HeroSection/HeroSection";
 import BasicLayout from "@/layouts/BasicLayout";
 
 export default function Home() {
   return (
     <BasicLayout>
-      <div>
-        
-      </div>
+      <HeroSection/>
+      
     </BasicLayout>
   );
 }
--- src/components/HomePage/HeroSection/HeroSection.tsx
+++ src/components/HomePage/HeroSection/HeroSection.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className='flex justify-center items-center py-40'>
+      <div className='max-w-screen-md w-full text-center flex flex-col justify-center items-center gap-5'>
+        <h2 className='font-normal text-7xl'>Advanced <br/>Elite AI</h2>
+        <p className='w-96 text-base opacity-90 font-thin leading-9'>Specialized AI for legal practices, professional services, and Fortune 500 companies.</p>
+        <Link 
+        className='text-base font-semibold bg-gradient-to-br from-white to-slate-300 text-secodary-color py-3 px-5 rounded-md'
+        href={'/contact'}>
+          Contact Us
+        </Link>
+      </div>
+    </div>
   )
 }
 
