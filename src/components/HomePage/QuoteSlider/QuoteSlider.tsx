f9019f907a0255a51dc438edadb37a278c4cc6e0
Vishnu <iam.vishnu.bhaskar@gmail.com>
2024-11-13 19:10:59 +0530

    Home page: Quote slider: catch drag start and drag end events.

--- src/components/HomePage/QuoteSlider/QuoteSlider.tsx
+++ src/components/HomePage/QuoteSlider/QuoteSlider.tsx
@@ -3,19 +3,44 @@
 import Image from "next/image"
 import slides from "./SliderItems"
 import { RiDoubleQuotesL } from "@remixicon/react";
+import { motion } from 'framer-motion';
+import { useState } from "react";
 
 const QuoteSlider = () => {
 
+  const [dragging, setDragging] = useState(false);
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const onDragStart = () => {
+    setDragging(true);
+    console.log('Start')
+  }
+
+  const onDragEnd = () => {
+    setDragging(false);
+    console.log('End')
+  }
+
   return (
     <div className="w-full overflow-hidden">
-      <div
-        className="flex gap-4">
+      <motion.div
+        drag="x"
+        dragConstraints={{
+          left: 0,
+          right: 0
+        }}
+        animate={{
+          translateX: `-${slideIndex}%`
+        }}
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
+        className="flex gap-0 cursor-grab active:cursor-grabbing">
         {
           slides.map((slide, index) => (
-            <div key={index} className={`flex md:flex-nowrap flex-col md:flex-row gap-5 min-w-[100vw]`}
-                  style={{
-                    backgroundColor: slide.bgColor
-                  }}
+            <div key={index} className={`flex md:flex-nowrap flex-col md:flex-row gap-5 min-w-[100%]`}
+              style={{
+                backgroundColor: slide.bgColor
+              }}
             >
               <div className="w-full md:w-3/5">
                 <Image
@@ -26,19 +51,20 @@ const QuoteSlider = () => {
                   height={0}
                   style={{
                     width: '100%',
-                    aspectRatio: '3/3'
+                    aspectRatio: '3/3',
                   }}
+                  className="[-webkit-user-drag:none]"
                 />
               </div>
               <div className="w-full h-full flex flex-col gap-0 justify-center p-10 md:p-20 text-secodary-color">
-                <p className="font-normal text-2xl md:text-4xl flex flex-nowrap gap-5 mb-6"><RiDoubleQuotesL widths={10}/>{slide.quote}</p>
+                <p className="font-normal text-2xl md:text-4xl flex flex-nowrap gap-5 mb-6"><RiDoubleQuotesL widths={10} />{slide.quote}</p>
                 <h2 className="font-semibold text-3xl md:text-xl">{slide.name}</h2>
                 <p className="font-light text-xl md:text-lg">{slide.position}</p>
               </div>
             </div>
           ))
         }
-      </div>
+      </motion.div>
     </div>
   )
 }
