9f4c5475598b893b0d057b19b6ee4a875f6f9a31
Vishnu <iam.vishnu.bhaskar@gmail.com>
2024-11-15 09:23:02 +1300

    AiPage: Header: MyAccount Popup finish

--- src/layouts/ChatbotLayout/Header/Header.tsx
+++ src/layouts/ChatbotLayout/Header/Header.tsx
@@ -1,11 +1,15 @@
-import { RiArrowDownSFill, RiArrowDropLeftLine, RiBarChartHorizontalLine } from "@remixicon/react"
+import { RiArrowDownSFill, RiBarChartHorizontalLine } from "@remixicon/react"
 import Link from "next/link"
-import { Dispatch, SetStateAction } from "react"
+import { Dispatch, SetStateAction, useState } from "react"
+import MyAccountPopup from "./MyAccountPopup"
 
 const Header = ({ sidebarOpen, setSidebarOpen }: Readonly<{
   sidebarOpen: boolean,
   setSidebarOpen: Dispatch<SetStateAction<boolean>>
 }>) => {
+
+  const [isMyAccountPopup, setIsMyAccountPop] = useState(false);
+
   return (
     <div className="py-4 px-5 flex flex-nowrap justify-between items-center absolute top-0 left-0 w-full">
       <div>
@@ -20,8 +24,11 @@ const Header = ({ sidebarOpen, setSidebarOpen }: Readonly<{
       </div>
       <div className="flex flex-nowrap gap-3">
         <Link href={'/'} className="flex items-center justify-start gap-1 bg-white px-4 rounded-md text-secodary-color text-sm font-semibold">Home</Link>
-        <div>
-          <button className="flex flex-nowrap gap-1 items-center hover:bg-[#ffffff15] py-2 px-4 rounded-lg">My Account <RiArrowDownSFill size={18} /></button>
+        <div className="relative">
+          <button 
+          onClick={() => setIsMyAccountPop(!isMyAccountPopup)}
+          className="flex flex-nowrap gap-1 items-center hover:bg-[#ffffff15] py-2 px-4 rounded-lg">My Account <RiArrowDownSFill size={18} /></button>
+          <MyAccountPopup isMyAccountPopup={isMyAccountPopup}/>
         </div>
       </div>
     </div>
