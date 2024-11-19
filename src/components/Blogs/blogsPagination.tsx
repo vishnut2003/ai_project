ec0715951178a1aed51e6fb6f15fe354845e06cd
ShenLongVansh <shenlong.codes@gmail.com>
2024-11-19 15:39:42 +0530

    test

--- src/components/Blogs/blogsPagination.tsx
+++ src/components/Blogs/blogsPagination.tsx
@@ -7,26 +7,31 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
+import { redirect } from "next/navigation";
 
 
 
 const BlogsPagination = ({ id }: { id: string }) => {
-  
-  return (
+
+    var intID = parseInt(id);
+    if(intID<=0){
+      redirect("/blogs/1")
+    }
+    return (
     
       <Pagination>
         <PaginationContent>
           <PaginationItem>
-            <PaginationPrevious href={`/blogs/${parseInt(id) - 1}`} />
+            <PaginationPrevious href={`/blogs/${intID - 1}`} />
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink className="text-black" isActive href={`/blogs/${id}`}>{id}</PaginationLink>
+            <PaginationLink className="text-black" isActive href={`/blogs/${intID}`}>{id}</PaginationLink>
           </PaginationItem>
           <PaginationItem>
             <PaginationEllipsis />
           </PaginationItem>
           <PaginationItem>
-            <PaginationNext href={`/blogs/${parseInt(id) + 1}`} />
+            <PaginationNext  href={`/blogs/${intID + 1}`} />
           </PaginationItem>
         </PaginationContent>
       </Pagination>
