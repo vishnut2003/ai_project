d1229ec42c95c118268edabf978b3f94f1263b84
ShenLongVansh <shenlong.codes@gmail.com>
2024-11-19 15:56:08 +0530

    blogs/3 error not fixed

--- src/components/Blogs/blogsPagination.tsx
+++ src/components/Blogs/blogsPagination.tsx
@@ -21,18 +21,38 @@ const BlogsPagination = ({ id }: { id: string }) => {
     
       <Pagination>
         <PaginationContent>
+          <div className="px-3">
           <PaginationItem>
             <PaginationPrevious href={`/blogs/${intID - 1}`} />
           </PaginationItem>
+          </div>
+            {intID > 1 && (
+            <div className="px-3">
+              <PaginationItem>
+              <PaginationLink className="text-white outline" href={`/blogs/${intID - 1}`}>
+                {intID - 1}
+              </PaginationLink>
+              </PaginationItem>
+            </div>
+            )}
+          
+      
           <PaginationItem>
-            <PaginationLink className="text-black" isActive href={`/blogs/${intID}`}>{id}</PaginationLink>
+            <PaginationLink className="text-black" isActive href={`/blogs/${intID}`}>{intID}</PaginationLink>
           </PaginationItem>
+          
+          
+          
+          <div className="px-3">
           <PaginationItem>
-            <PaginationEllipsis />
+            <PaginationLink className="text-white outline"  href={`/blogs/${(intID+1)}`}>{(intID+1)}</PaginationLink>
           </PaginationItem>
+          </div>
+          <div className="px-3">
           <PaginationItem>
             <PaginationNext  href={`/blogs/${intID + 1}`} />
           </PaginationItem>
+          </div>
         </PaginationContent>
       </Pagination>
     
