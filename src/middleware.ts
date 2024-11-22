c96ed47dd821dbf959bab944bd2d3958af352de0
ShenLongVansh <shenlong.codes@gmail.com>
2024-11-22 10:03:40 +0530

    updated the middleware that was resolved yesterday

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,14 @@
-import { authkitMiddleware } from '@workos-inc/authkit-nextjs';
+import { authkitMiddleware } from "@workos-inc/authkit-nextjs";
 
-export default authkitMiddleware();
-
+export default authkitMiddleware({
+  middlewareAuth: {
+    enabled: true,
+    // Allow logged out users to view these paths
+    unauthenticatedPaths: ["/"],
+  },
+});
 // Match against pages that require authentication
 // Leave this out if you want authentication on every page in your application
-export const config = { matcher: ['/','/ai'] };
+export const config = {
+  matcher: ["/((?!api|_next/static|.*\\..*|_next/image|favicon.ico).*)"],
+};
