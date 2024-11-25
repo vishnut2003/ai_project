0172ae2a7703dc1e095d20256a53d81ebe0e0d08
ShenLongVansh <shenlong.codes@gmail.com>
2024-11-25 11:48:23 +0530

    fixed the dynamic routes and forced sign ins on non existing pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ export default authkitMiddleware({
     middlewareAuth: {
         enabled: true,
         // Allow logged out users to view these paths
-        unauthenticatedPaths: ['/'],
+        unauthenticatedPaths: ['/','/contact','/blogs/:path*','/:path*'],
     },
 });
 
