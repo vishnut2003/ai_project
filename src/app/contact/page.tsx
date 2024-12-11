7c53ddebee18f9132ce52a7dbf97bc2870c432d9
Vishnu <iam.vishnu.bhaskar@gmail.com>
2024-12-11 15:33:02 +0530

    ContactPage: Started working.

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,16 @@
+import BasicLayout from '@/layouts/BasicLayout'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <BasicLayout>
+        <div className='flex w-full justify-center items-center px-8 py-14'>
+            <div className='w-full max-w-screen-xl'>
+                <h2 className='text-3xl font-semibold'>Contact Us</h2>
+            </div>
+        </div>
+    </BasicLayout>
+  )
+}
+
+export default Contact
