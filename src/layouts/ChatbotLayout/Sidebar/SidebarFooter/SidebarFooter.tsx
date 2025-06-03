adea4b04ed5b4c82b2802e8a9a2dbeab13801959
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-06-03 14:47:52 +0530

    Hide Links -> Judgement & Acts and laws, add pricing page link in upgrade to plus button  in Chatbot layout -> Sidebar Footer

--- src/layouts/ChatbotLayout/Sidebar/SidebarFooter/SidebarFooter.tsx
+++ src/layouts/ChatbotLayout/Sidebar/SidebarFooter/SidebarFooter.tsx
@@ -1,6 +1,6 @@
 import { clientSignout } from "@/components/Header/AuthComponents/MobileSidebarComponents/clientSignout"
-import { RiAuctionLine, RiLogoutBoxRLine, RiScales3Line, RiUser6Line } from "@remixicon/react"
-import Link from "next/link"
+import { RiLogoutBoxRLine, RiUser6Line } from "@remixicon/react"
+import Link from "next/link";
 import { useActionState } from "react"
 
 const SidebarFooter = () => {
@@ -13,25 +13,28 @@ const SidebarFooter = () => {
             <div className="flex flex-col gap-5 h-full min-h-0">
 
                 {/* Upgrade to pro plan */}
-                <div className="flex justify-start items-center gap-3 min-h-0 h-full">
+                <Link 
+                    className="flex justify-start items-center gap-3 min-h-0 h-full"
+                    href={'/dashboard/pricing'}
+                >
                     <RiUser6Line size={20} />
                     <p className="text-sm font-light w-full flex justify-between items-center m-0 whitespace-nowrap">
                         Upgrade to plus
                         <span className="py-1 px-4 bg-yellow-200 text-black rounded-lg text-xs font-semibold">New</span>
                     </p>
-                </div>
+                </Link>
 
                 {/* Judgements Button */}
-                <Link href={'#'} className="flex justify-start gap-3 min-h-0 h-full">
+                {/* <Link href={'#'} className="flex justify-start gap-3 min-h-0 h-full">
                     <RiAuctionLine size={20} />
                     <p className="text-sm font-light m-0 whitespace-nowrap">Judgements</p>
-                </Link>
+                </Link> */}
                 
                 {/* Acts and Laws Button */}
-                <Link href={'#'} className="flex justify-start gap-3 min-h-0 h-full">
+                {/* <Link href={'#'} className="flex justify-start gap-3 min-h-0 h-full">
                     <RiScales3Line size={20} />
                     <p className="text-sm font-light m-0 whitespace-nowrap">Acts and Laws</p>
-                </Link>
+                </Link> */}
 
                 {/* Logout button */}
                 <form action={signoutAction} className="flex justify-start gap-3 min-h-0 h-full">
