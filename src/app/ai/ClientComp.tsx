aacbf2f3e598ae6bcf4a6bde661901b44121c6f8
Shashi <shashibabu880465@gmail.com>
2025-08-18 14:11:47 +0530

    Add canonical url to path: /ai

---
+++ src/app/ai/ClientComp.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import ConversationTemplate from "@/components/AiPage/Conversation/ConversationTemplate";
+import IntroPromptArea from "@/components/AiPage/IntroPromptArea/IntroPromptArea";
+import PromptTextarea from "@/components/AiPage/PromptTextarea/PromptTextarea";
+import ConversationInterface from "@/interfaces/conversation";
+import ChatbotLayout from "@/layouts/ChatbotLayout/ChatbotLayout";
+import { Suspense, useState } from "react";
+
+export default function AiPageClientComponent() {
+    const [chatId, setChatId] = useState<string>('')
+    const [conversation, setConversation] = useState<ConversationInterface[]>([{ from: "ai_assistant", message: "Type your prompt to start!" }]);
+    const [requestInProgress, setRequestInProgress] = useState<boolean>(false);
+
+    return (
+        <ChatbotLayout>
+            {
+                conversation.length <= 1 ?
+                    <IntroPromptArea
+                        conversation={conversation}
+                        setConversation={setConversation}
+                        setRequestInProgress={setRequestInProgress}
+                        chatId={chatId}
+                        setChatId={setChatId}
+                    />
+                    :
+                    <>
+                        <ConversationTemplate conversation={conversation} requestInProgress={requestInProgress} />
+                        <Suspense>
+                            <PromptTextarea
+                                setConversation={setConversation}
+                                conversation={conversation}
+                                setRequestInProgress={setRequestInProgress}
+                                chatId={chatId}
+                                setChatId={setChatId}
+                            />
+                        </Suspense>
+                    </>
+            }
+        </ChatbotLayout>
+    )
+}
--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -1,45 +1,16 @@
-'use client';
 
-import ConversationTemplate from "@/components/AiPage/Conversation/ConversationTemplate";
-import IntroPromptArea from "@/components/AiPage/IntroPromptArea/IntroPromptArea";
-import PromptTextarea from "@/components/AiPage/PromptTextarea/PromptTextarea"
-import ConversationInterface from "@/interfaces/conversation";
-import ChatbotLayout from "@/layouts/ChatbotLayout/ChatbotLayout"
-import { Suspense, useState } from "react"
+import { Metadata } from "next";
+import AiPageClientComponent from "./ClientComp";
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: "https://ailawgpt.com/ai",
+  },
+};
+
 
 const Page = () => {
-
-  const [chatId, setChatId] = useState<string>('')
-  const [conversation, setConversation] = useState<ConversationInterface[]>([{ from: "ai_assistant", message: "Type your prompt to start!" }]);
-  const [requestInProgress, setRequestInProgress] = useState<boolean>(false);
-
-  return (
-    <ChatbotLayout>
-      {
-        conversation.length <= 1 ?
-          <IntroPromptArea
-            conversation={conversation}
-            setConversation={setConversation}
-            setRequestInProgress={setRequestInProgress}
-            chatId={chatId}
-            setChatId={setChatId}
-          />
-          :
-          <>
-            <ConversationTemplate conversation={conversation} requestInProgress={requestInProgress} />
-            <Suspense>
-              <PromptTextarea
-                setConversation={setConversation}
-                conversation={conversation}
-                setRequestInProgress={setRequestInProgress}
-                chatId={chatId}
-                setChatId={setChatId}
-                 />
-            </Suspense>
-          </>
-      }
-    </ChatbotLayout>
-  )
+  return <AiPageClientComponent/>
 }
 
 export default Page;
