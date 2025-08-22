f4bebeea6344acab60932b983ece0bd2dedf7cca
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-08-22 20:10:18 +0530

    Hide Signout button from dashboard sidebar layout if the user is not logged in;

--- src/layouts/ChatbotLayout/Sidebar/SidebarFooter/SidebarFooter.tsx
+++ src/layouts/ChatbotLayout/Sidebar/SidebarFooter/SidebarFooter.tsx
@@ -1,11 +1,23 @@
 import { clientSignout } from "@/components/Header/AuthComponents/MobileSidebarComponents/clientSignout"
+import { authVerify } from "@/utils/client/authHelper";
 import { RiLogoutBoxRLine, RiUser6Line } from "@remixicon/react"
 import Link from "next/link";
-import { useActionState } from "react"
+import { useActionState, useEffect, useState } from "react"
 
 const SidebarFooter = () => {
 
     const [signoutState, signoutAction] = useActionState(clientSignout, null);
+    const [isLoggedin, setIsLoggedin] = useState<boolean>(false);
+
+    useEffect(() => {
+        (async () => {
+            const session = await authVerify();
+
+            if (session.user) {
+                setIsLoggedin(true);
+            }
+        })()
+    }, [])
 
     return (
         <div className="min-h-max">
@@ -13,7 +25,7 @@ const SidebarFooter = () => {
             <div className="flex flex-col gap-5 h-full min-h-0">
 
                 {/* Upgrade to pro plan */}
-                <Link 
+                <Link
                     className="flex justify-start items-center gap-3 min-h-0 h-full"
                     href={'/dashboard/pricing'}
                 >
@@ -29,20 +41,23 @@ const SidebarFooter = () => {
                     <RiAuctionLine size={20} />
                     <p className="text-sm font-light m-0 whitespace-nowrap">Judgements</p>
                 </Link> */}
-                
+
                 {/* Acts and Laws Button */}
                 {/* <Link href={'#'} className="flex justify-start gap-3 min-h-0 h-full">
                     <RiScales3Line size={20} />
                     <p className="text-sm font-light m-0 whitespace-nowrap">Acts and Laws</p>
                 </Link> */}
 
-                {/* Logout button */}
-                <form action={signoutAction} className="flex justify-start gap-3 min-h-0 h-full">
-                    <button type="submit" className="flex justify-start gap-3 min-h-0">
-                        { !signoutState && <RiLogoutBoxRLine size={20} />}
-                        <p className="text-sm font-light m-0 whitespace-nowrap">Sign out</p>
-                    </button>
-                </form>
+                {/* Logout button */
+
+                    isLoggedin &&
+                    <form action={signoutAction} className="flex justify-start gap-3 min-h-0 h-full">
+                        <button type="submit" className="flex justify-start gap-3 min-h-0">
+                            {!signoutState && <RiLogoutBoxRLine size={20} />}
+                            <p className="text-sm font-light m-0 whitespace-nowrap">Sign out</p>
+                        </button>
+                    </form>
+                }
             </div>
         </div>
     )
