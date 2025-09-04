67da086dd74423a85557573ace34d506c8cc5999
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-09-04 12:12:52 +0530

    Added Signin button in sidebar and hide upgrade to pro button if the user is not logged in;

--- src/layouts/ChatbotLayout/Sidebar/SidebarFooter/SidebarFooter.tsx
+++ src/layouts/ChatbotLayout/Sidebar/SidebarFooter/SidebarFooter.tsx
@@ -1,3 +1,4 @@
+import ClientLoginButton from "@/components/Header/AuthComponents/ClientLoginButton";
 import { clientSignout } from "@/components/Header/AuthComponents/MobileSidebarComponents/clientSignout"
 import { authVerify } from "@/utils/client/authHelper";
 import { RiLogoutBoxRLine, RiUser6Line } from "@remixicon/react"
@@ -24,17 +25,20 @@ const SidebarFooter = () => {
             {/* footer actions */}
             <div className="flex flex-col gap-5 h-full min-h-0">
 
-                {/* Upgrade to pro plan */}
-                <Link
-                    className="flex justify-start items-center gap-3 min-h-0 h-full"
-                    href={'/dashboard/pricing'}
-                >
-                    <RiUser6Line size={20} />
-                    <p className="text-sm font-light w-full flex justify-between items-center m-0 whitespace-nowrap">
-                        Upgrade to plus
-                        <span className="py-1 px-4 bg-yellow-200 text-black rounded-lg text-xs font-semibold">New</span>
-                    </p>
-                </Link>
+                {/* Upgrade to pro plan */
+                    isLoggedin &&
+                    <Link
+                        className="flex justify-start items-center gap-3 min-h-0 h-full"
+                        href={'/dashboard/pricing'}
+                    >
+                        <RiUser6Line size={20} />
+                        <p className="text-sm font-light w-full flex justify-between items-center m-0 whitespace-nowrap">
+                            Upgrade to plus
+                            <span className="py-1 px-4 bg-yellow-200 text-black rounded-lg text-xs font-semibold">New</span>
+                        </p>
+                    </Link>
+                }
+
 
                 {/* Judgements Button */}
                 {/* <Link href={'#'} className="flex justify-start gap-3 min-h-0 h-full">
@@ -50,13 +54,19 @@ const SidebarFooter = () => {
 
                 {/* Logout button */
 
-                    isLoggedin &&
-                    <form action={signoutAction} className="flex justify-start gap-3 min-h-0 h-full">
-                        <button type="submit" className="flex justify-start gap-3 min-h-0">
-                            {!signoutState && <RiLogoutBoxRLine size={20} />}
-                            <p className="text-sm font-light m-0 whitespace-nowrap">Sign out</p>
-                        </button>
-                    </form>
+                    isLoggedin ?
+                        <form action={signoutAction} className="flex justify-start gap-3 min-h-0 h-full">
+                            <button type="submit" className="flex justify-start gap-3 min-h-0">
+                                {!signoutState && <RiLogoutBoxRLine size={20} />}
+                                <p className="text-sm font-light m-0 whitespace-nowrap">Sign out</p>
+                            </button>
+                        </form>
+                        :
+                        <div
+                            className="w-full flex justify-center"
+                        >
+                            <ClientLoginButton />
+                        </div>
                 }
             </div>
         </div>
